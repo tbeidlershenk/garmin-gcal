bf1d42d08584afffaedc8eed38a57ca1fcb8c2da
Tobias Beidler-Shenk <tbeidlershenk@gmail.com>
2024-09-27 15:49:24 -0400

    Added logger dependency

--- add_sleep_event.py
+++ add_sleep_event.py
@@ -6,6 +6,7 @@ import datetime
 import os
 from dotenv import load_dotenv
 import sys
+from logger import logger
 
 load_dotenv()
 SCOPES = ['https://www.googleapis.com/auth/calendar']
@@ -23,7 +24,8 @@ day = datetime.date.today() if len(
 data = api.get_sleep_data(day.isoformat())
 
 if data['dailySleepDTO']['id'] is None:
-    exit(f"Error: No sleep data found for {day}")
+    logger.info(f"Error: No sleep data found for {day}")
+    exit(0)
 
 start_time_ms = data['dailySleepDTO']['sleepStartTimestampGMT']
 end_time_ms = data['dailySleepDTO']['sleepEndTimestampGMT']
@@ -48,15 +50,18 @@ try:
     credentials = Credentials.from_service_account_file(
         credentials_file, scopes=SCOPES)
     service = build('calendar', 'v3', credentials=credentials)
+    logger.info("Built Google Calendar service")
 
     # query for events on the calendar
     events_result = service.events().list(
         calendarId=calendar_id).execute()
     events = events_result.get('items', [])
+    logger.info("Retrieved existing calendar events")
 
     # script already run for today
     if calendar_has_events_on_day(events, day):
-        exit(f"Error: Event already exists for {day}")
+        logger.info(f"Error: Event already exists for {day}")
+        exit(0)
 
     event = {
         'summary': "Garmin",
@@ -71,7 +76,7 @@ try:
 
     # add the event to configured calendar
     event = service.events().insert(calendarId=calendar_id, body=event).execute()
-    print(f"Event created: {event.get('htmlLink')}")
+    logger.info(f"Event created: {event.get('htmlLink')}")
 
 except Exception as error:
-    print(f"Error: {error}")
+    logger.info(f"Error: {error}")
--- init_api.py
+++ init_api.py
@@ -2,6 +2,7 @@ from garminconnect import Garmin, GarminConnectAuthenticationError
 from garth.exc import GarthHTTPError
 import os
 import requests
+from logger import logger
 
 
 def get_mfa():
@@ -14,35 +15,34 @@ def init_api(email, password, tokenstore, tokenstore_base64):
     """Initialize Garmin API with your credentials."""
 
     try:
-        print(
-            f"Trying to login to Garmin Connect using token data from directory '{tokenstore}'...\n")
+        logger.info(
+            f"Trying to login to Garmin Connect using token data from directory '{tokenstore}'...")
 
         garmin = Garmin()
         garmin.login(tokenstore)
 
     except (FileNotFoundError, GarthHTTPError, GarminConnectAuthenticationError):
         # Session is expired. You'll need to log in again
-        print(
-            "Login tokens not present, login with your Garmin Connect credentials to generate them.\n"
-            f"They will be stored in '{tokenstore}' for future use.\n")
+        logger.info(
+            f"Login tokens not present, login with your Garmin Connect credentials to generate them. They will be stored in '{tokenstore}' for future use.")
         try:
             garmin = Garmin(email=email, password=password,
                             is_cn=False, prompt_mfa=get_mfa)
             garmin.login()
             # Save Oauth1 and Oauth2 token files to directory for next login
             garmin.garth.dump(tokenstore)
-            print(
-                f"Oauth tokens stored in '{tokenstore}' directory for future use. (first method)\n")
+            logger.info(
+                f"Oauth tokens stored in '{tokenstore}' directory for future use. (first method)")
             # Encode Oauth1 and Oauth2 tokens to base64 string and safe to file for next login (alternative way)
             token_base64 = garmin.garth.dumps()
             dir_path = os.path.expanduser(tokenstore_base64)
             with open(dir_path, "w") as token_file:
                 token_file.write(token_base64)
-            print(
-                f"Oauth tokens encoded as base64 string and saved to '{dir_path}' file for future use. (second method)\n"
+            logger.info(
+                f"Oauth tokens encoded as base64 string and saved to '{dir_path}' file for future use. (second method)"
             )
         except (FileNotFoundError, GarthHTTPError, GarminConnectAuthenticationError, requests.exceptions.HTTPError) as err:
-            print("Error occurred during Garmin Connect login.")
+            logger.info("Error occurred during Garmin Connect login.")
             return None
 
     return garmin
